05cadbb23cb30ab35adf9abac09d64c82bb7c7bf
Sunny0084 <papdi.marton084@gmail.com>
2026-01-16 22:11:25 +0100

    updaste

--- mods/Glitcs/scripts/shitfartpoopvomitcrapfeces.lua
+++ mods/Glitcs/scripts/shitfartpoopvomitcrapfeces.lua
@@ -1,4 +1,4 @@
-local songs = {'how-to-be', 'borin', 'almmost', 'szkrics', 'pessimism', 'quote-and-quote-depressed', 'ron-and-shaggy-randomly-sings-against-prehistoric-lizard'}
+local songs = {'how-to-be', 'szkrics', 'pessimism', 'borin', 'quote-and-quote-depressed', 'benger', 'almmost', 'ron-and-shaggy-randomly-sings-against-prehistoric-lizard'}
 
 function onUpdate()
     if getPropertyFromClass('flixel.FlxG', 'keys.justPressed.SPACE') then
--- mods/Glitcs/stages/osu.lua
+++ mods/Glitcs/stages/osu.lua
@@ -2,7 +2,7 @@ function onCreate()
 	makeLuaSprite('osu', 'osu', 100, 270);
 	setScrollFactor('osu', 1, 1);
 
-	addLuaSprite('osu', false);
+	addLuaSprite('osu', true);
 
 	close(true); --For performance reasons, close this script once the stage is fully loaded, as this script won't be used anymore after loading the stage
 end
