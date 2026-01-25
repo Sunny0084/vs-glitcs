14627e7eaae21a4500afdd4cef3beeb9ed9659c0
Sunny0084 <papdi.marton084@gmail.com>
2026-01-25 21:22:50 +0100

    mania mode fix + NINCS TÖBB credits

---
+++ mods/scripts/shitfartpoopvomitcrapfeces.lua
@@ -0,0 +1,10 @@
+local songs = {
+    'how-to-be','szkrics','pessimism', 'borin', 'quote-and-quote-depressed',
+    'benger','almmost', 'ron-and-shaggy-randomly-sings-against-prehistoric-lizard'
+}
+
+function onUpdate()
+    if getPropertyFromClass('flixel.FlxG', 'keys.justPressed.SPACE') then
+        loadSong(songs[getRandomInt(1, #songs)])
+    end
+end
