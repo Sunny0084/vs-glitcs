144024f7c1ee063dd564df378113709201b91ab4
Sunny0084 <papdi.marton084@gmail.com>
2026-05-13 16:57:29 +0200

    nvm nincs uj skin

--- mods/Glitcs/custom_notetypes/Mania.lua
+++
@@ -1,7 +0,0 @@
-function onCreate()
-	for i = 0, getProperty('unspawnNotes.length') - 1 do
-		if getPropertyFromGroup('unspawnNotes', i, 'noteType') == 'Mania' then
-			setPropertyFromGroup('unspawnNotes', i, 'texture', 'noteSkins\\mania');
-		end
-	end
-end
